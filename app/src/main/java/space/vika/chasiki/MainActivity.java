5cd38fc4d7b60d0f55f7e5d8c3ebcf88df1da8cc
DanilPustovalov <Kir_Fall@mail.ru>
2022-02-16 20:37:32 +0300

    Итог для самсунга

--- app/src/main/java/space/vika/chasiki/MainActivity.java
+++ app/src/main/java/space/vika/chasiki/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
     SensorManager sensorManager;
     SensorEventListener sensorEventListener;
 
+    int hours,minutes,seconds,weekday, date;
+    float battery;
+    @ColorInt int color= Color.parseColor("#DDDDDD");
+    @ColorInt int color1= Color.parseColor("#707070");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,15 +43,35 @@ public class MainActivity extends AppCompatActivity {
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         sensorManager.registerListener(sensorEventListener, light, sensorManager.SENSOR_DELAY_NORMAL);
 
-
+        TextView textView = findViewById(R.id.Time);
+        TextView textView2 = findViewById(R.id.Date);
+        TextView textView3 = findViewById(R.id.Battery);
         r=new Runnable() {
             @Override
             public void run() {
                 time.setToNow();
-                DrawingView dv = new DrawingView(MainActivity.this,
-                        time.hour,time.minute,time.second,time.weekDay,time.monthDay,getBatteryLevel()
-                );
-                setContentView(dv);
+
+                hours=time.hour+3;
+                minutes=time.minute;
+                seconds = time.second;
+                weekday = time.weekDay;
+                date= time.monthDay;
+                battery=getBatteryLevel();
+
+                String text=String.format("%02d:%02d:%02d",hours,minutes,seconds);
+                String[] day_of_week={"ПОН","ВТ","СР","ЧТ","ПТ","СБ","ВС"};
+                String text2=String.format("ДАТА: %s %d",day_of_week[weekday],date);
+                String batteryLevel = "БАТАРЕЯ: "+(int) battery+ "%";
+
+                textView.setTextColor(color);
+                textView.setTextSize(36);
+                textView.setText(text);
+                textView2.setTextColor(color);
+                textView2.setTextSize(24);
+                textView2.setText(text2);
+                textView3.setTextColor(color);
+                textView3.setTextSize(24);
+                textView3.setText(batteryLevel);
                 handler.postDelayed(r,1000);
             }
         };
@@ -62,73 +87,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return ((float) level/(float) scale)* 100.0f;
     }
-    public class DrawingView extends View {
-        //Typeface tf;
-        Paint textPaint,textBackPaint;
-        int hours,minutes,seconds,weekday, date;
-        float battery;
-        @ColorInt int color= Color.parseColor("#DDDDDD");
-        @ColorInt int color1= Color.parseColor("#707070");
-        public DrawingView(Context context,int hours, int minutes, int seconds,int weekday,int date,float battery) {
-            super(context);
-            textPaint=new Paint();
-            textPaint.setColor(color);
-            textPaint.setAntiAlias(true);
-            textPaint.setTextAlign(Paint.Align.CENTER);
-            textPaint.setTextSize(getResources().getDimension(R.dimen.text_size));
-            textBackPaint=new Paint();
-            textBackPaint.setColor(color1);
-            textBackPaint.setAntiAlias(true);
-            textBackPaint.setTextAlign(Paint.Align.CENTER);
-            textBackPaint.setTextSize(getResources().getDimension(R.dimen.text_size));
-            this.hours=hours+3;
-            this.minutes=minutes;
-            this.seconds=seconds;
-            this.weekday=weekday;
-            this.date=date;
-            this.battery=battery;
 
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            float width=canvas.getWidth();
-            float height=canvas.getHeight();
-            float centerX=width/2f;
-            float centerY=height/2f;
-            int cur_hour= hours;
-            if(cur_hour>23){
-                cur_hour=0;
-            }
-            sensorEventListener=new SensorEventListener() {
-                @Override
-                public void onSensorChanged(SensorEvent event) {
-                    if(event.values[0]>100){
-                        canvas.drawColor(Color.WHITE);
-
-                    }else {
-                        canvas.drawColor(Color.BLACK);
-
-                    }
-                }
-
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-                }
-            };
-            String text=String.format("%02d:%02d:%02d",cur_hour,minutes,seconds);
-            String[] day_of_week={"ПОН","ВТ","СР","ЧТ","ПТ","СБ","ВС"};
-            String text2=String.format("ДАТА: %s %d",day_of_week[weekday],date);
-            String batteryLevel = "БАТАРЕЯ: "+(int) battery+ "%";
-            //canvas.drawColor(Color.BLACK);
-            textPaint.setColor(color1);
-            textPaint.setTextSize(getResources().getDimension(R.dimen.text_size));
-            canvas.drawText(text,centerX,centerY,textPaint);
-            textPaint.setTextSize(getResources().getDimension(R.dimen.text_size_small));
-            canvas.drawText(batteryLevel+" "+text2,centerX,centerY+getResources().getDimension(R.dimen.text_size_small),textPaint);
-
-        }
-    }
 }
